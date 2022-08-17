5a2826463d9b800fce8cf9d36f5087361b33ae43
Evan Rosson <rossonevan7281@gmail.com>
2022-08-17 15:32:19 -0500

    commit tree 5bfeae77e02825606d9f87b38748240b0d6b1091

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map do |c|
+            "#{c.actor.first_name} #{c.actor.last_name}"
+        end
+    end
+
 end
